1328b75a21ea5bbce401e667f8ce768230bd855e
damian <dwoz137@gmail.com>
2023-05-07 20:39:55 +0200

    PokeApi config

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'pokeapi' => [
+        'v2' => [
+            'url' => env('POKEAPI_V2_URL'),
+        ],
+    ],
 ];
